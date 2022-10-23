acd975ca8f9ab361138d6c89bb310b5df4fd0b0a
JHSeo <qnfqnfqnf@gmail.com>
2022-10-23 19:37:57 +0900

    feat: add sidebar style with logo

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,29 @@
+import Link from 'next/link';
+
+import LogoIcon from '../__icons/Logo.icon';
+
 export interface SidebarProps {}
 
 function Sidebar(props: SidebarProps) {
-  return <div className="bg-base-100 w-[80vw] max-w-sm">Sidebar</div>;
+  return (
+    <div className="bg-base-100 w-[80vw] max-w-sm">
+      <div className="px-6 py-5 border-b">
+        <LogoIcon height="24" />
+      </div>
+      <ul className="px-6 py-4">
+        <li>
+          <Link href="/" passHref>
+            <a className="block py-4">홈</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/new" passHref>
+            <a className="block py-4">추가</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
