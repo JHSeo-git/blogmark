9600ef31434e4f5742b25d060153241c7686d27b
JHSeo <qnfqnfqnf@gmail.com>
2022-10-24 21:44:42 +0900

    feat: add nextauth options type

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import type { NextApiHandler } from 'next';
+import type { NextAuthOptions } from 'next-auth';
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 
@@ -12,7 +13,7 @@ if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET) {
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
 
-const options = {
+const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
