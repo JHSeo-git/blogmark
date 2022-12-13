b6228dc00fee20e03c74a33b37b7ee4a76e6584c
JHSeo <qnfqnfqnf@gmail.com>
2022-12-13 21:42:20 +0900

    update Card Skeleton styles

--- src/components/Card/Card.Skeleton.tsx
+++ src/components/Card/Card.Skeleton.tsx
@@ -4,8 +4,10 @@ function CardSkeleton() {
       <div className="animate-pulse bg-base-200 rounded-lg overflow-hidden">
         <div className="w-full aspect-video" />
       </div>
-      <div className="animate-pulse mt-3 h-8 w-1/2 bg-base-200 rounded-lg" />
+      <div className="animate-pulse mt-3 h-6 w-1/3 bg-base-200 rounded-lg" />
+      <div className="animate-pulse mt-2 h-8 w-1/2 bg-base-200 rounded-lg" />
       <div className="animate-pulse mt-1 h-6 w-4/5 bg-base-200 rounded-lg" />
+      <div className="animate-pulse mt-1 h-6 w-1/12 ml-auto bg-base-200 rounded-lg" />
     </div>
   );
 }
