64d2809e79650bba56bdbdb8bcc9c42023205e30
JHSeo <qnfqnfqnf@gmail.com>
2022-12-18 23:32:09 +0900

    bump: next-auth@4.18.6

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,10 +1,10 @@
 import ApiClient from '../api-client';
 
-if (!process.env.NEXT_PUBLIC_API_URL) {
-  throw new Error('Please define the NEXT_PUBLIC_API_URL environment variable');
+if (!process.env.NEXT_PUBLIC_APP_URL) {
+  throw new Error('Please define the NEXT_PUBLIC_APP_URL environment variable');
 }
 
-const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
 
 const client = new ApiClient(baseUrl);
 
